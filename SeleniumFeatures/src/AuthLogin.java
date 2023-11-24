bfaf9d0704287ea89410cccf9979babee2cbb3b8
Yurii Bartash <uyriy18@gmail.com>
2023-11-24 17:32:39 +0200

    cross browser testing added

---
+++ SeleniumFeatures/src/AuthLogin.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class AuthLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://storefront:Oreo2022@dwdev.boohooman.com/");
+
+	}
+
+}
